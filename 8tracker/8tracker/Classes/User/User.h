b7eaefaab10048e2da3a559f688ca3d1ece09376
Nick Brice <nick.brice88@gmail.com>
2012-11-30 00:06:20 -0800

    Stubs for User class

---
+++ 8tracker/8tracker/Classes/User/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  8tracker
+//
+//  Created by Nick Brice on 11/30/12.
+//  Copyright (c) 2012 Nick Brice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ 8tracker/8tracker/Classes/User/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  8tracker
+//
+//  Created by Nick Brice on 11/30/12.
+//  Copyright (c) 2012 Nick Brice. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
