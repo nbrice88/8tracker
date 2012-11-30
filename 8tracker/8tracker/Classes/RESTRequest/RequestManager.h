25f11ffacb37757c1f14366a9f95328e4470409e
Nick Brice <nick.brice88@gmail.com>
2012-11-30 02:49:11 -0800

    Added stubs for RequestManager.*

---
+++ 8tracker/8tracker/Classes/RESTRequest/RequestManager.h
@@ -0,0 +1,13 @@
+//
+//  RequestManager.h
+//  8tracker
+//
+//  Created by Nick Brice on 11/30/12.
+//  Copyright (c) 2012 Nick Brice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RequestManager : NSObject
+
+@end
---
+++ 8tracker/8tracker/Classes/RESTRequest/RequestManager.m
@@ -0,0 +1,13 @@
+//
+//  RequestManager.m
+//  8tracker
+//
+//  Created by Nick Brice on 11/30/12.
+//  Copyright (c) 2012 Nick Brice. All rights reserved.
+//
+
+#import "RequestManager.h"
+
+@implementation RequestManager
+
+@end
